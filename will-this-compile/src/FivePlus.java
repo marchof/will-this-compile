c951b3c904db1aba18ae8ebba903414e6ae0b817
Marc R. Hoffmann <hoffmann@mountainminds.com>
2013-08-05 22:24:25 +0200

    Initial commit with some examples.

---
+++ will-this-compile/src/FivePlus.java
@@ -0,0 +1,17 @@
+/**
+ * Five plus signs in a row? Will this compile and if yes, what will be
+ * the result?
+ */
+public class FivePlus {
+	
+	public static void main(String[] args) {
+		
+		int x = 20;
+		
+		int s = x ++ + ++ x;
+		
+		System.out.println(s);
+
+	}
+
+}
