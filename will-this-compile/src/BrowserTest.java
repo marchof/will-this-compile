95328df60920ceb49040ba6d7723e47e49e4b83b
Marc R. Hoffmann <hoffmann@mountainminds.com>
2013-08-19 16:43:58 +0200

    My favourite from Java Puzzlers.

---
+++ will-this-compile/src/BrowserTest.java
@@ -0,0 +1,12 @@
+/**
+ * Chapter 22 "Dupe of URL" from the book "Java Puzzlers".
+ * 
+ * Copyright 2004-2005, Joshua Bloch and Neal Gafter
+ */
+public class BrowserTest {
+    public static void main(String[] args) {
+        System.out.print("iexplore:");
+        http://www.google.com;
+        System.out.println(":maximize"); 
+    }
+}
