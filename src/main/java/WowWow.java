9dbd2ebe22d8920e2596c86880a4803fa8d00b20
Marc R. Hoffmann <hoffmann@mountainminds.com>
2024-01-26 17:20:27 +0000

    Add Toot by Lukas Eder

---
+++ src/main/java/WowWow.java
@@ -0,0 +1,18 @@
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
+/**
+ * Toot https://mastodon.social/@lukaseder/111816246912988799
+ * 
+ * Copyright Lukas Eder 
+ */
+public class WowWow {
+
+    @Wow Wow @Wow [] @Wow [] wow(@Wow Wow [] wow @Wow []) @Wow [] @Wow [] {
+        return new Wow @Wow [] @Wow [] @Wow [] @Wow [] {{ wow }};
+    }
+    
+    @Target ({TYPE_USE})
+    @interface Wow {}
+
+}
